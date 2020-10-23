fb4d2a80c88173617a6d95e537ed1d1b7895b3f0
Jeff <jlgranof@icloud.com>
2020-10-22 23:24:47 -0500

    deployed to heroku and set up the models

---
+++ star_wars_events.md
@@ -0,0 +1,11 @@
+## Categories
+
+## Events
+1. Order 66 (@Jedi Temple)
+2. Podracing Championships (Extra upgrade to sit with tuskan raiders)
+3. Death Star Unvieling (@Alderan)
+4. Republic Leader Kill Coleseum @ Geonosis
+5. Carbonite Museum Opening Night
+6. Squid Lake by the Mon Calamri Ballet at Coruscant Opera House (Optional tickets for private box w supreme chancellor)
+7. Kessel Run Tour
+8. 
--- starter_app/__init__.py
+++ starter_app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, session
 from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_migrate import Migrate
 
 
 from starter_app.models import db, User
@@ -10,10 +11,10 @@ from starter_app.api.user_routes import user_routes
 from starter_app.config import Config
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 db.init_app(app)
+Migrate(app, db)
 
 ## Application Security
 CORS(app)
