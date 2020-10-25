df099d339ea1beafa31efdea8e0c8d8da6e43c80
kmcgee92 <kaseycmcgee@gmail.com>
2020-10-25 01:45:23 -0600

    altered landing page

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -4,14 +4,215 @@ import React from 'react';
 import Header from '../Header/Header'
 import CarouselComponent from '../CarouselComponent/CarouselComponent'
 
+// @material-ui/core
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    extrasTemporary: {
+        zIndex: 100,
+        height: "200px",
+        width: "50%",
+        margin: "0 auto",
+        backgroundColor: "blue",
+    },
+})
+
 
 const LandingPage = () => {
-
+    const classes = useStyles()
     
     return (
         <>
             <Header/>
             <CarouselComponent/>
+
+            <div classeName={classes.extrasTemporary}>
+                Featured Events
+                <div>
+                    Events 
+                </div>
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                Events 
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+            <div>
+                About the Site Maybe
+            </div>
+
         </>
     );
 };
