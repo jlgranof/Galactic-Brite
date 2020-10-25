4e2be9e1119aa08b5600dafa85cc7397d2f89634
Jeff <jlgranof@icloud.com>
2020-10-25 18:12:02 -0500

    got backend for all events to work with basic setup

--- backend/api/event_routes.py
+++ backend/api/event_routes.py
@@ -5,4 +5,17 @@ event_routes = Blueprint('events', __name__)
 
 @event_routes.route('/')
 def index():
-    pass
+    response = Event.query.all()
+    events = []
+    for event in response:
+        events.append({
+            "name": event.name,
+            "event_description": event.event_description,
+            "host": event.user.username,
+            "event_date": event.event_date,
+            "event_planet": event.event_planet,
+            "event_picture_url": event.event_picture_url,
+            "category": event.category.type,
+            "is_featured": event.is_featured
+        })
+    return jsonify(events)
