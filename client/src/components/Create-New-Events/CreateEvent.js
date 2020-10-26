6e8f2e89c63fc57670c1113bbf33a499987f2788
James White <whitejamesthe2nd@gmail.com>
2020-10-26 15:56:41 -0400

    fixing slow loading times

---
+++ client/src/components/Create-New-Events/CreateEvent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// core components
+import Header from '../Header/Header'
+// import CarouselComponent from '../CarouselComponent/CarouselComponent'
+
+const CreateEventForm = () => {
+    // const classes = useStyles()
+
+    return (
+        <>
+            <div>
+                <Header/>
+            </div>
+            <div>
+                <form method="POST">
+                    <label>Suck it noob</label>
+                    <input type='text'></input>
+                </form>
+            </div>
+        </>
+    )
+}
+export default CreateEventForm;
