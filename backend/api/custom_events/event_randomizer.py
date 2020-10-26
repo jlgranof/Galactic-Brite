e1b7acac6657715fb67dddd7742f67323d2dd658
Jeff <jlgranof@icloud.com>
2020-10-26 18:03:51 -0500

    got most of random events done

---
+++ backend/api/custom_events/event_randomizer.py
@@ -0,0 +1,16 @@
+from random import randint
+import requests
+from description import random_description
+from date import random_date
+from name import random_name
+
+event = {
+    "name": random_name(),
+    "event_description": random_description(),
+    "host_id": randint(1,9),
+    "event_date": random_date(),
+    "event_planet": requests.get(f"http://swapi.dev/api/planets/{randint(1,60)}/").json()['name'],
+    "is_featured": False
+}
+
+print(event)
