4011d15d65583ebdae718fae2b6c74b4ec7cf709
alec <alecbgarcia@gmail.com>
2020-10-26 17:08:30 -0700

    new jwt

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,6 +4,13 @@ from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_migrate import Migrate
 
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies
+)
+
 
 from backend.models import db, User
 from backend.api.user_routes import user_routes
@@ -14,11 +21,18 @@ from backend.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+app.config['JWT_SECRET_KEY'] = 'C1D55AF87585F574A7C7566ED281D'
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(event_routes, url_prefix='/api/events')
 app.register_blueprint(session_routes, url_prefix='/api/session')
+
 db.init_app(app)
 Migrate(app, db)
+jwt = JWTManager(app)
 
 ## Application Security
 CORS(app)
